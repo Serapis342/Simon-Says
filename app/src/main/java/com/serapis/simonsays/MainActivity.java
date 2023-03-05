196860e693a25a5844051dc0286c1536e8f58226
serapis <MyPrivateSpam@protonmail.com>
2023-03-05 11:45:59 +0100

    Updated activity_main.xml and MainActivity.java

--- app/src/main/java/com/serapis/simonsays/MainActivity.java
+++ app/src/main/java/com/serapis/simonsays/MainActivity.java
@@ -5,10 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    Button green_btn;
+    Button red_btn;
+    Button yellow_btn;
+    Button blue_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        green_btn = findViewById(R.id.green_btn);
+        red_btn = findViewById(R.id.red_btn);
+        yellow_btn = findViewById(R.id.yellow_btn);
+        blue_btn = findViewById(R.id.blue_btn);
     }
 }
